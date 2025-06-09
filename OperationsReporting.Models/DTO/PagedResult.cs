a2ad1c5945e006c603680144dbf69d099cfda27f
Iskren Stanchev <Iskren.Stanchev@procreditbank.bg>
2025-06-09 20:57:53 +0300

    refactor paginated result placed properly

--- OperationsReporting.Models/DTO/PagedResult.cs
+++ OperationsReporting.Models/DTO/PagedResult.cs
@@ -1,13 +1,13 @@
-﻿namespace OperationsReporting.Models.Filters
+﻿namespace OperationsReporting.Models.DTO
 {
     public class PagedResult<T>
     {
         public int Page { get; set; }
-        
+
         public int PageSize { get; set; }
-        
+
         public int TotalCount { get; set; }
-        
+
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
 
         public List<T> Items { get; set; } = new();
