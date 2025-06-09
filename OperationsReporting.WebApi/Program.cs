0189383b3cb649673f4ad9ce1c9322cc3f226d02
Iskren Stanchev <Iskren.Stanchev@procreditbank.bg>
2025-06-09 22:59:15 +0300

    refactor program settup

--- OperationsReporting.WebApi/Program.cs
+++ OperationsReporting.WebApi/Program.cs
@@ -1,52 +1,8 @@
-using OperationsReporting.Services.Interfaces;
-using OperationsReporting.Services;
-using Microsoft.EntityFrameworkCore;
-using OperationsReporting.DAL;
-using OperationsReporting.Services.Mapping;
-using OperationsReporting.DAL.Interfaces;
-using OperationsReporting.DAL.Repositories;
-using OperationsReporting.WebApi.Middleware;
+using OperationsReporting.WebApi;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddDbContext<OperationsReportingContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// AutoMapper
-builder.Services.AddAutoMapper(typeof(TransactionMappingProfile).Assembly);
-
-//services
-builder.Services.AddScoped<IMerchantService, MerchantService>();
-builder.Services.AddScoped<IPartnerService, PartnerService>();
-builder.Services.AddScoped<ITransactionService, TransactionService>();
-builder.Services.AddScoped<IExportService, ExportService>();
-
-// repositories
-builder.Services.AddScoped<IPartnerRepository, PartnerRepository>();
-builder.Services.AddScoped<IMerchantRepository, MerchantRepository>();
-builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseMiddleware<GlobalErrorHandler>();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+var app = WebApplication
+                .CreateBuilder(args)
+                .ConfigureServices()
+                .ConfigurePipeline();
 
 app.Run();
---
+++ OperationsReporting.WebApi/ProgramSettup.cs
@@ -0,0 +1,60 @@
+﻿using OperationsReporting.DAL.Interfaces;
+using OperationsReporting.DAL.Repositories;
+using OperationsReporting.DAL;
+using OperationsReporting.Services.Interfaces;
+using OperationsReporting.Services.Mapping;
+using OperationsReporting.Services;
+using Microsoft.EntityFrameworkCore;
+using OperationsReporting.WebApi.Middleware;
+
+
+namespace OperationsReporting.WebApi
+{
+    internal static class ProgramSettup
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<OperationsReportingContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // AutoMapper
+            builder.Services.AddAutoMapper(typeof(TransactionMappingProfile).Assembly);
+
+            //services
+            builder.Services.AddScoped<IMerchantService, MerchantService>();
+            builder.Services.AddScoped<IPartnerService, PartnerService>();
+            builder.Services.AddScoped<ITransactionService, TransactionService>();
+            builder.Services.AddScoped<IExportService, ExportService>();
+
+            // repositories
+            builder.Services.AddScoped<IPartnerRepository, PartnerRepository>();
+            builder.Services.AddScoped<IMerchantRepository, MerchantRepository>();
+            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+            return builder.Build();
+        }
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseMiddleware<GlobalErrorHandler>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
