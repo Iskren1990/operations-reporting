c0b1247ef067cb2b9a2186b5868b26662759a2f6
Iskren Stanchev <Iskren.Stanchev@procreditbank.bg>
2025-06-10 20:49:13 +0300

    refactor preseeded data extracted in separate class

--- OperationsReporting.DAL/OperationsReportingContext.cs
+++ OperationsReporting.DAL/OperationsReportingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OperationsReporting.DAL.Seed;
 using OperationsReporting.Models.Entities;
 
 namespace OperationsReporting.DAL
@@ -35,36 +36,7 @@ namespace OperationsReporting.DAL
                 .HasIndex(t => t.ExternalId)
                 .IsUnique();
 
-            modelBuilder.Entity<Partner>().HasData(new Partner
-            {
-                Id = 1,
-                Name = "Test Partner"
-            });
-
-            modelBuilder.Entity<Merchant>().HasData(
-                new Merchant
-                {
-                    Id = 1,
-                    Name = "Merchant A",
-                    BoardingDate = new DateTime(2023, 05, 01, 0, 0, 0, DateTimeKind.Utc),
-                    Url = "https://merchant-a.com",
-                    Country = "Netherlands",
-                    AddressMain = "Main Street 123",
-                    AddressSecondary = "Suite 4B",
-                    PartnerId = 1
-                },
-                new Merchant
-                {
-                    Id = 2,
-                    Name = "Merchant B",
-                    BoardingDate = new DateTime(2023, 04, 01, 0, 0, 0, DateTimeKind.Utc),
-                    Url = "https://merchant-b.com",
-                    Country = "Bulgaria",
-                    AddressMain = "Secondary Street 45",
-                    AddressSecondary = null,
-                    PartnerId = 1
-                }
-            );
+            modelBuilder.Seed();
         }
     }
 }
---
+++ OperationsReporting.DAL/Seed/InitialDataSeed.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OperationsReporting.Models.Entities;
+
+namespace OperationsReporting.DAL.Seed
+{
+    internal static class InitialDataSeed
+    {
+        internal static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Partner>().HasData(new Partner
+            {
+                Id = 1,
+                Name = "Test Partner"
+            });
+
+            modelBuilder.Entity<Merchant>().HasData(
+                new Merchant
+                {
+                    Id = 1,
+                    Name = "Merchant A",
+                    BoardingDate = new DateTime(2023, 05, 01, 0, 0, 0, DateTimeKind.Utc),
+                    Url = "https://merchant-a.com",
+                    Country = "Netherlands",
+                    AddressMain = "Main Street 123",
+                    AddressSecondary = "Suite 4B",
+                    PartnerId = 1
+                },
+                new Merchant
+                {
+                    Id = 2,
+                    Name = "Merchant B",
+                    BoardingDate = new DateTime(2023, 04, 01, 0, 0, 0, DateTimeKind.Utc),
+                    Url = "https://merchant-b.com",
+                    Country = "Bulgaria",
+                    AddressMain = "Secondary Street 45",
+                    AddressSecondary = null,
+                    PartnerId = 1
+                }
+            );
+        }
+    }
+}
